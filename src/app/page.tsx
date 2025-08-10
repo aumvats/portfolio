eff657d41f3b39fd1e246fa1264df5e1f4d32f03
Aum Vats <theaumvats@gmail.com>
2025-08-11 01:56:39 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,7 +89,7 @@ export default function Page() {
           ))}
         </div>
       </section>
-      <section id="education">
+{/*       <section id="education">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 7}>
             <h2 className="text-xl font-bold">Education</h2>
@@ -111,7 +111,7 @@ export default function Page() {
             </BlurFade>
           ))}
         </div>
-      </section>
+      </section> */}
       <section id="skills">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 9}>
